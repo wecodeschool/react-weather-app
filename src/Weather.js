e6f4312c8a3c849adebaab9c02053d3af4ac3725
Matt Delac <matt@delac.io>
2019-11-20 15:59:56 +0000

    Added Weather forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -59,6 +60,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+        <WeatherForecast city={weatherData.city} />
       </div>
     );
   } else {
---
+++ src/WeatherForecast.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data.list.slice(0, 5));
+    setLoaded(true);
+  }
+
+  function formatHours(date) {
+    let hours = date.getHours();
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast row">
+        {forecast.map(function(weather) {
+          return (
+            <div className="col">
+              {formatHours(new Date(weather.dt * 1000))}
+              <WeatherIcon code={weather.weather[0].icon} />
+              {Math.round(weather.main.temp)}°C
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    const apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+}
