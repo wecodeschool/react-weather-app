59e832dc02fa2d7e8daa5f4244977b25f0aa1baa
Matt Delac <matt@delac.io>
2019-11-20 12:39:13 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.delac.io/" target="_blank">
-          Matt Delac
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/wecodeschool/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.delac.io/" target="_blank">
+            Matt Delac
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/wecodeschool/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
