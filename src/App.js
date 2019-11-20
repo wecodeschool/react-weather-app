43ba02df83c2375df052d2a6cf4cfcf06d52c5bd
Matt Delac <matt@delac.io>
2019-11-20 12:24:17 +0000

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.delac.io/" target="_blank">
+          Matt Delac
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/wecodeschool/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
