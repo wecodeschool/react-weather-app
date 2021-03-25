a0f5531c62a3e33fd91e801cb36a2d5992a7e1e9
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-25 12:40:55 +0000

    Add snapshots

---
+++ _snapshots/forecast-layout/src/WeatherForecast.js
@@ -0,0 +1,20 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-day">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19°</span>
+            <span className="WeatherForecast-temperature-min">10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
