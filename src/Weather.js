d1417b4e1d29053b8259d23d359a651c28f1e385
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-25 09:48:23 +0000

    Remove old forecast

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
-import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -60,7 +59,6 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast city={weatherData.city} />
       </div>
     );
   } else {
