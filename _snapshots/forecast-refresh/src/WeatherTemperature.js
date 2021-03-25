474722b84706ee9f96fe515210ee46823a36c0cc
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-25 13:51:32 +0000

    Add snapshots

---
+++ _snapshots/forecast-refresh/src/WeatherTemperature.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">
+        °C 
+      </span>
+    </div>
+  );
+}
