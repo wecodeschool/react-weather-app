5f16c06d078be9bde745badc2d40d4a0d2354120
Matt Delac <matthieu.aussaguel@gmail.com>
2021-03-25 13:29:02 +0000

    5 day forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -16,9 +16,15 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
