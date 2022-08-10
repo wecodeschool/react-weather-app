0519c65430b9cc791f3310270abed2edefdb9c4c
Matt Delac <matthieu.aussaguel@gmail.com>
2022-08-10 15:22:34 +0100

    upgrade to react 18

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -15,12 +15,12 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
-            <div className="float-left">
+          <div className="d-flex">
+            <div>
               <WeatherIcon code={props.data.icon} size={52} />
             </div>
 
-            <div className="float-left">
+            <div>
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
