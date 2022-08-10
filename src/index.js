99d1136d0c10779cca855843572895bea4049df1
Matt Delac <matthieu.aussaguel@gmail.com>
2022-08-10 15:28:02 +0100

    Update snapshots

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -15,8 +14,3 @@ root.render(
   </StrictMode>
 );
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
